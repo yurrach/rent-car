0dceb37b90881e9ee5f6995b63d47ebae9d09845
yurrach <y.rachkouski@gmail.com>
2018-01-06 15:09:29 +0300

    create validation for add car component

--- src/app/admin/add-edit-car/add-edit-car.component.ts
+++ src/app/admin/add-edit-car/add-edit-car.component.ts
@@ -3,8 +3,6 @@ import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 import { AdminCarService } from '../shared/services/admin-car.service';
 import { Observable } from 'rxjs/Observable';
 
-
-
 @Component({
   selector: 'crayf-add-edit-car',
   templateUrl: './add-edit-car.component.html',
@@ -26,7 +24,6 @@ export class AddEditCarComponent implements OnInit {
     trim: this.trimsList$
   };
 
-
   constructor(
     private fb: FormBuilder,
     private adminCarService: AdminCarService
@@ -35,10 +32,11 @@ export class AddEditCarComponent implements OnInit {
   ngOnInit() {
     this.yearsList = this.getYearsList(2000);
     this.createCarForm();
-    
-
   }
-
+  isInvalid(name) {
+    const control = this.carForm.controls[name];
+    return control.touched && control.invalid;
+   }
   resetForm(ctrlArr, index) {
     const resetValue = {};
     ctrlArr.forEach((ctrl, i) => {
@@ -46,90 +44,43 @@ export class AddEditCarComponent implements OnInit {
         resetValue[ctrl] = this.carForm.controls[ctrl].value;
       }
     })
-
     this.carForm.reset(resetValue);
-
   }
+
   resetOptionsList(index) {
     const optArr = Object.keys(this.optionsListObj);
     optArr.forEach((opt, i) => {
       if (i > index) {
         this[`${opt}sList$`] = Observable.of(null);
-
       }
     })
-
-
   }
 
   onControlChange({ name }) {
     const ctrlArr = Object.keys(this.carForm.controls);
     const index = ctrlArr.indexOf(name);
-    
-
-    if (this.carForm.controls[ctrlArr[index + 1]].value) {
-      this.resetForm(ctrlArr, index);
-      this.resetOptionsList(index);
-
-    }
     if (name === 'trim') {
       return;
     }
-    const listName = {
-      year: 'make_display',
-      make: 'model_name',
-      model: 'model_trim',
-    };
-    const observableListName = ctrlArr[index + 1] + 'sList$';
-    this[observableListName] = this.getListFromApi(listName[name]);
-  }
+    if (this.carForm.controls[ctrlArr[index + 1]].value) {
+      this.resetForm(ctrlArr, index);
+      this.resetOptionsList(index);
+    }
+    if (this.carForm.controls[name].valid) {
+      
+      const listName = {
+        year: 'make_display',
+        make: 'model_name',
+        model: 'model_trim',
+      };
+      const observableListName = ctrlArr[index + 1] + 'sList$';
+      this[observableListName] = this.getListFromApi(listName[name]);
+    }
+    }
 
   onSubmit() {
 
   }
-
-  onYearChange() {
-    if (this.carForm.controls.make.value) {
-      // this.carForm.reset({ year: this.carForm.controls.year.value });
-      // this.makesList$ = Observable.of(null);
-      // this.modelsList$ = Observable.of(null);
-      // this.trimsList$ = Observable.of(null);
-      
-    }
-    // this.getMakesList();
-  }
-  onMakeChange() {
-    if (this.carForm.controls.model.value) {
-      // this.modelsList$ = Observable.of(null);
-      // this.trimsList$ = Observable.of(null);
-/*       this.carForm.reset({
-        year: this.carForm.controls.year.value,
-        make: this.carForm.controls.make.value
-      }); */
-
-    }
-    // this.getModelsList();
-  }
-  onModelChange() {
-    if (this.carForm.controls.trim.value) {
-      // this.trimsList$ = Observable.of(null);
-
-    }
-
-  }
-
-  onTrimChange() {
-
-      // this.carForm.reset({
-      //   year: this.carForm.controls.year.value,
-      //   make: this.carForm.controls.make.value,
-      //   model: this.carForm.controls.model.value,
-      //   trim: this.carForm.controls.trim.value
-      // });
-      this.isTrimSelected = true;
-
-  }
-
   getYearsList(startYear): number[] {
     const yearsList = [];
     const endYear = (new Date()).getFullYear();
@@ -139,7 +90,7 @@ export class AddEditCarComponent implements OnInit {
     return yearsList;
   }
   
-  getMakesList() {
+/*   getMakesList() {
     const listName = 'make_display';
     this.makesList$ = this.getListFromApi(listName);
   }
@@ -150,17 +101,18 @@ export class AddEditCarComponent implements OnInit {
   getTrimsList() {
     const listName = 'model_trim';
     this.trimsList$ = this.getListFromApi(listName);
-  }
+  } */
+
   getListFromApi(listName) {
     return this.adminCarService.getCarParamsList$(this.carForm.value, listName);
   }
 
   createCarForm() {
     this.carForm = this.fb.group({
-      year: [null, Validators.required],
-      make: [null, Validators.required],
-      model: [null, Validators.required],
-      trim: [null, Validators.required],
+      year: ['', Validators.required],
+      make: ['', Validators.required],
+      model: ['', Validators.required],
+      trim: ['', Validators.required],
       isSale: [null],
       carImages: this.fb.array([this.fb.control(null)])
     });
--- src/app/admin/shared/services/admin-car.service.ts
+++ src/app/admin/shared/services/admin-car.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/publishLast';
+
 
 @Injectable()
 export class AdminCarService {
@@ -8,16 +11,26 @@ export class AdminCarService {
     private http: HttpClient
   ) { }
 
-  getCarParamsList$({year, make, model}, listName) {
+  getCarParamsList$({ year, make, model }, listName) {
+    Observable
     const baseApiUrl = `https://www.carqueryapi.com/api/0.3/?callback=JSONP_CALLBACK&cmd=get`;
+
     const jsonpCallback = `JSONP_CALLBACK`;
     const makeQuery = 'Makes&year=' + year;
     const modelQuery = 'Models&year=' + year + '&make=' + make;
     const trimQuery = 'Trims&year='+year+'&make='+make+'&model='+model;
-    const query = model ? trimQuery : make ? modelQuery : makeQuery;
-    return this.http.jsonp(baseApiUrl + query, jsonpCallback).map(res => {
-      const paramsArray: [any] = Object.values(res)[0];
+    const query = {
+      'make_display': makeQuery,
+      'model_name': modelQuery,
+      'model_trim': trimQuery
+    }
+    // const query = model ? trimQuery : make ? modelQuery : makeQuery;
+
+    return this.http.jsonp(baseApiUrl + query[listName], jsonpCallback)
+      .map(res => {
+        console.log('change');  
+        const paramsArray: [any] = Object.values(res)[0];
       return paramsArray.map(param => param[listName]);
-    });
+      }).publishLast().refCount();
   }
 }
