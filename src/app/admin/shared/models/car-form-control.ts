6dc7dee4784f956faaa4b4cc13e737aadc3ab4e7
yurrach <y.rachkouski@gmail.com>
2018-01-07 17:13:30 +0300

    create car-form-control model

---
+++ src/app/admin/shared/models/car-form-control.ts
@@ -0,0 +1,15 @@
+import { Observable } from 'rxjs/Observable';
+
+export class CarFormControl {
+  public name: string;
+  public placeholder: string;
+  public label: string;
+  constructor({ name, placeholder, label }) {}
+}
+export class CarSelectFormControl extends CarFormControl {
+  public optionsList$: Observable<string[] | number[]>;
+  constructor({ name, placeholder, label }) {
+    super({ name, placeholder, label });
+    this.optionsList$ = Observable.of(null);
+  }
+}
